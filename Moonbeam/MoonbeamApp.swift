e189edb02b2710da4f7a616bd4ce6d76d7cadefa
jack <82819874+slooops@users.noreply.github.com>
2025-06-04 15:32:38 -0700

    Initial Commit

---
+++ Moonbeam/MoonbeamApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonbeamApp.swift
+//  Moonbeam
+//
+//  Created by jasloop on 6/4/25.
+//
+
+import SwiftUI
+
+@main
+struct MoonbeamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoonbeamTests/MoonbeamTests.swift
@@ -0,0 +1,16 @@
+//
+//  MoonbeamTests.swift
+//  MoonbeamTests
+//
+//  Created by jasloop on 6/4/25.
+//
+
+import Testing
+
+struct MoonbeamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
